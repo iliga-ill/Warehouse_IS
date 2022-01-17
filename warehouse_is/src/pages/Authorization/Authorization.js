1a3c8f2bfa840252db72079beb29b15505941540
iliga-ill <illvolkov@mail.ru>
2022-01-17 18:37:14 +0300

    changes

---
+++ warehouse_is/src/pages/Authorization/Authorization.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from "react";
+import './Authorization.css';
+import Table from "../../components/Table/Table";
+
+const styles = {
+
+  }
+
+  
+
+export default function Authorization(props){
+
+    return (
+        <>
+        <div>Авторизируйтесь!</div>
+        </>
+    )
+}
