b127746e869e3bbc214ae90e90a4bc13e2815511
usc5h_743pg2c <uc00510144@yandex.ru>
2022-02-02 09:18:57 +0300

    Add new queries

--- warehouse_is/src/pages/LogisticianOrders/LogisticianOrders.js
+++ warehouse_is/src/pages/LogisticianOrders/LogisticianOrders.js
@@ -30,6 +30,10 @@ export default function LogisticianOrders(props){
         setReload(reload+1)
     }
 
+    React.useEffect(() => {
+        apiGetOrders()
+    }, [reload]);
+
     var [tabs, setTabs] = React.useState([
         {id:0, selected: true, title: "Текущие"},
         {id:1, selected: false, title: "Выполненные"}
@@ -51,11 +55,38 @@ export default function LogisticianOrders(props){
     //-------------------------------------------------------------------------Табы конец
     //-------------------------------------------------------------------------Блок 1
 
-    const [orders, setOrders] = React.useState([{id:0, text: "Ничего не найдено", selected: true, code: 0}])
+    //const [orders, setOrders] = React.useState([{id:0, text: "Ничего не найдено", selected: true, code: 0}])
+    const [orders, setOrders] = React.useState([])
     React.useEffect(() => {
-        //if (goodsType != []) apiGetGoodsByShipmentOrder()
+        if (orders.length > 0) apiGetOrders()
     }, [orders]);
     
+    function apiGetOrders() {
+        var xhr = new XMLHttpRequest();
+        var status = 'complited'
+        tabs.forEach(tab => {
+            if (tab.selected) {
+                if (tab.title == "Текущие") status = 'in progress'
+                else status = 'complited'
+            }
+           
+        });
+        xhr.open('GET', host+'/orders_all'+'?'+`status=${status}`, true);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == XMLHttpRequest.DONE) {
+            var answer = JSON.parse(this.response)
+            var buffer = []
+            answer.map(function( element, i) {
+                buffer.push({id:getId(), text: element.name, selected: false, code: element.id})
+            });
+            setOrders(buffer)
+            apiGetOrderGoods()
+          }
+        }
+        
+        xhr.send(null);
+    }
+
     //-------------------------------------------------------------------------Блок 1 конец
 
     //-------------------------------------------------------------------------Блок 2
@@ -78,6 +109,8 @@ export default function LogisticianOrders(props){
     var edit_column = {add:true, edit:true, delete:true}
 
     const [tableList, setTableList] = React.useState([{number:1, shipmentNumber:"Доставка №0000001", shipmentDate:"2022-01-14", shipmentCost:1000, shipmentStatus:"Ожидается"}])
+
+
         
     //-------------------------------------стол 1 конец
     //-------------------------------------стол 2
@@ -91,7 +124,8 @@ export default function LogisticianOrders(props){
     ]) 
     var edit_column1 = {add:true, edit:true, delete:true}
 
-    const [tableList1, setTableList1] = React.useState([{number:1, goodsType:"bb", weight:100, expectingAmount:10, realAmount:10}])
+    //const [tableList1, setTableList1] = React.useState([{number:1, goodsType:"bb", weight:100, expectingAmount:10, realAmount:10}])
+    const [tableList1, setTableList1] = React.useState([])
         
     //-------------------------------------стол 2 конец
     
@@ -117,7 +151,36 @@ export default function LogisticianOrders(props){
     ])
     var edit_column2 = {add:false, edit:false, delete:false}
 
-    const [tableList2, setTableList2] = React.useState([{number:1, goodsType:"bb", goodsCost:100, shipmentProgress:"10/100", weight:10}])
+    // const [tableList2, setTableList2] = React.useState([{number:1, goodsType:"bb", goodsCost:100, shipmentProgress:"10/100", weight:10}])
+    const [tableList2, setTableList2] = React.useState([])
+
+    function apiGetOrderGoods() {
+        var order = ''
+        orders.forEach(element => {
+          if (element.selected == true) order = element
+        });
+        if (order != '') {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', host+'/orders_goods'+ "?" + `order_id=${order.code}`, true);
+            
+            xhr.onreadystatechange = function() {
+              if (xhr.readyState == XMLHttpRequest.DONE) {
+                var answer = JSON.parse(this.response)
+                console.log("ManagerSellProducts apiGetOrderGoods answer: ")
+                console.log(answer)
+                var buffer = []
+                answer.map(function( element, i) {
+                    buffer.push({number:i+1, goodsType: element.name, weight: element.weight, goodsCost: element.price, shipmentProgress:"10/100", })
+                    buffer[i].id = getId()
+                    buffer[i].code = element.code;
+                });
+                setTableList2(buffer)
+              }
+            }
+            
+            xhr.send(null);
+        }
+    }
     //-------------------------------------стол 3 конец
     //-------------------------------------------------------------------------Блок 3 конец
 //#endregion
@@ -139,7 +202,7 @@ export default function LogisticianOrders(props){
                     <div style={{width:300+'px', display:'inline-table'}} >
                         <TableComponent height={250} columns={tableHeaders1} rows={tableList1} setNewTableList={setTableList1} editColumn={edit_column1}/>
                     </div>
-                    <div style={{height:20+"px"}} />
+                    <div style={{height:20+"px"}} />    
                     <div class="place_holder"/><button class="bt_send" onClick={btn_send_1}>Принять</button>
                 </FlexibleBlock>
                 <FlexibleBlock>
@@ -148,6 +211,7 @@ export default function LogisticianOrders(props){
                         <div class="low_text bold">Крайний срок поставки:&nbsp;&nbsp;<label class="normal">{shipmentDeadline}</label></div>
                         <div class="low_text bold">Полная&nbsp;стоимость&nbsp;заказа:&nbsp;<label class="normal">{orderCost}</label></div>
                         <div class="low_text bold">Поставщик:&nbsp;<label class="normal">{provider}</label></div>
+                        <div class="low_text bold">Товары&nbsp;в&nbsp;заказе:&nbsp;</div>
                     </div>
                     <div style={{width:300+'px', display:'inline-table'}} >
                         <TableComponent height={300} columns={tableHeaders2} rows={tableList2} setNewTableList={setTableList2} editColumn={edit_column2}/>
